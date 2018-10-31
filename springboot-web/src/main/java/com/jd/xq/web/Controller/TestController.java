4298e40376e3fc507feaa49625d3824d5c091e01
duanxiaoqiu <Dxq#5201314>
2018-10-31 18:19:25 +0800

    集成日志

--- springboot-web/src/main/java/com/jd/xq/web/Controller/TestController.java
+++ springboot-web/src/main/java/com/jd/xq/web/Controller/TestController.java
@@ -1,5 +1,8 @@
 package com.jd.xq.web.Controller;
 
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -16,9 +19,17 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/test")
 public class TestController {
 
+    private static final Logger logger = LoggerFactory.getLogger(TestController.class);
+
     @RequestMapping("/index")
     @ResponseBody
     public String index() {
+        logger.trace("这是trace日志...");
+        logger.debug("这是debug日志...");
+        //SpringBoot默认给我们使用的是info级别的，没有指定级别的就用SpringBoot默认规定的级别；root级别
+        logger.info("这是info日志...");
+        logger.warn("这是warn日志...");
+        logger.error("这是error日志...");
         return "test";
     }
 
